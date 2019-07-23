b09587fb02b0f509be8e596f27a1fe391ae9a704
Bradley Grainger <bgrainger@gmail.com>
2019-07-23 11:47:36 -0700

    Release 0.11.0.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const keytarService = 'clone-leeroy';
 const keytarAccount = 'github-token';
 let gitHubAccessToken = '';
 
-console.log('clone-leeroy 0.10.3');
+console.log('clone-leeroy 0.11.0');
 
 getGitHubAccessToken()
   .then(token => {
